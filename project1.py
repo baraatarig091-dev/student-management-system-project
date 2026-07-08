d99d6974e4268dd54d187efed7f2d1657ae503d6
baraatarig091-dev <baraatarig091@gmail.com>
2026-07-08 11:08:23 +0200

    Add files via upload

--- project1.py
+++ project1.py
@@ -6,3 +6,15 @@ print("4: update  Student ")
 print("5: delete  Student ")
 print("6: Exit")
 choice =int(input("Enter your choice:"))
+if choice == 1:
+    print("add student ")
+elif choice == 2:
+    print(" display student ")
+elif choice == 3:
+    print(" search  Student ")
+elif choice == 4:
+    print(" update  Student ")
+elif choice == 5:
+    print(" delete  Student ")
+else:
+    print("Exit ")
